da117f3766500af634bfb523e15f7dbacf5cc62d
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-02-19 01:34:36 -0300

    add src/envs

---
+++ src/envs.js
@@ -0,0 +1,28 @@
+const debug = require('debug')('apogeu:envs');
+const fs = require('fs');
+const getBase = require('./getBase');
+
+const defaultEnvs = {
+  node_env: 'development',
+  port: 3000,
+  timestamps: true,
+  logger: 'dev',
+  mongodb: `mongodb://localhost:27017/${getBase('').replace(/.*\//, '')}`,
+};
+
+const envsPath = getBase('config/envs');
+let envs;
+
+try {
+  fs.statSync(envsPath);
+  envs = require(envsPath);
+} catch (e) {
+  debug(`${envsPath} not fouond`);
+  envs = {};
+}
+
+envs = Object.assign(defaultEnvs, envs);
+
+debug(envs);
+
+module.exports = envs;
