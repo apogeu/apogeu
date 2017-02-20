aa6c9862a26b358e7bd9d923f4f4270ba6f0cd09
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-02-20 09:50:57 -0300

    fix: eslint bin/new

--- bin/new.js
+++ bin/new.js
@@ -2,14 +2,17 @@
 
 const debug = require('debug')('apogeu:new');
 const path = require('path');
+const log = require('winston');
+
 const createDir = require('../src/createDir');
 const download = require('../src/downloadSource');
 const createPackage = require('../src/createPackage');
 const installDependencies = require('../src/installDependencies');
+
 const scriptFolder = process.cwd();
 
 module.exports = (project_name) => {
-  let projectFolder = project_name ? path.join(scriptFolder, project_name) : scriptFolder;
+  const projectFolder = project_name ? path.join(scriptFolder, project_name) : scriptFolder;
   project_name = project_name || path.basename(scriptFolder);
 
   debug(`project name: ${project_name}`);
@@ -21,7 +24,7 @@ module.exports = (project_name) => {
     .then(() => installDependencies(projectFolder))
     .then(() => debug(`${project_name} app created`))
     .catch((err) => {
-      console.error(err.stack);
+      log.error(err.stack);
       process.exit(1);
     });
 };
