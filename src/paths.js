9fbcbc09568eadb5474a4a8e1e86dd1e16ca464d
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2017-02-21 05:41:14 -0300

    fixed templates path / add config self dir

--- src/paths.js
+++ src/paths.js
@@ -1,29 +1,22 @@
 const debug = require('debug')('apogeu:paths');
 
-const getPath = (folder) => {
-  const template = process.env.TEMPLATE;
-  let path = `./${folder}`;
-  if (template) path = `./${template}/${folder}`;
-  debug(path);
-  return path;
-};
-
 module.exports = {
-  models: getPath('app/models'),
-  controllers: getPath('app/controllers'),
-  services: getPath('app/services'),
-  views: getPath('app/views'),
-  i18n: getPath('app/i18n'),
-  scaffold: getPath('config/scaffold'),
+  models: 'app/models',
+  controllers: 'app/controllers',
+  services: 'app/services',
+  views: 'app/views',
+  i18n: 'app/i18n',
   config: {
-    routes: getPath('config/routes'),
-    envs: getPath('config/envs'),
+    this: 'config',
+    routes: 'config/routes',
+    envs: 'config/envs',
+    scaffold: 'config/scaffold',
   },
   assets: {
-    stylesheets: getPath('app/assets/stylesheets'),
+    stylesheets: 'app/assets/stylesheets',
   },
   public: {
-    this: getPath('public'),
-    stylesheets: getPath('public/stylesheets'),
+    this: 'public',
+    stylesheets: 'public/stylesheets',
   },
 };
