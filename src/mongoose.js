366d6ce15da44975a9ea489a19edeb1d34a2cea2
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2017-02-21 05:42:57 -0300

    mongoose config passed to the app project

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,41 +1,8 @@
-const debug = require('debug')('apogeu:mongoose');
-const mongoose = require('mongoose');
-const log = require('winston');
-const mongooseTimestamps = require('mongoose-timestamp');
+const path = require('path');
+const paths = require('../src/paths');
+const getBase = require('../src/getBase');
 
-const envs = require('./envs');
+const databasePath = getBase(path.join(paths.config.this, 'database.js'));
+const database = require(databasePath);
 
-const { timestamps, mongodb, node_env } = envs;
-
-debug(`mongoose timestamps: ${timestamps}`);
-
-mongoose.Promise = Promise;
-
-if (timestamps) mongoose.plugin(mongooseTimestamps);
-
-module.exports = () => new Promise((resolve, reject) => {
-  const debugMongoose = node_env !== 'production';
-  debug(`debug mongoose: ${debugMongoose}`);
-
-  mongoose.set('debug', debugMongoose);
-
-  mongoose.connect(mongodb);
-
-  mongoose.connection.on('connected', () => log.info(`Mongoose default connection open to ${mongodb}`));
-
-  mongoose.connection.on('error', err => reject(err));
-
-  mongoose.connection.on('disconnected', () => log.info('Mongoose default connection disconnected'));
-
-  mongoose.connection.once('open', () => {
-    resolve();
-    log.info('Mongoose default connection is open');
-  });
-
-  process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-      log.info('Mongoose default connection disconnected through app termination');
-      process.exit(0);
-    });
-  });
-});
+module.exports = database;
