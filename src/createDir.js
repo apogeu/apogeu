310c2e51208c845b711a3f480291e68ebf504253
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-02-22 20:34:28 -0300

    improved createdir debug

--- src/createDir.js
+++ src/createDir.js
@@ -5,12 +5,12 @@ const rimraf = require('rimraf');
 const log = require('winston');
 
 module.exports = (dir, overwrite) => {
-  debug(`create dir: ${dir}`);
   try {
     if (fs.existsSync(dir)) {
       if (!overwrite) return;
       rimraf.sync(dir);
     }
+    debug(dir);
     mkdirp.sync(dir);
   } catch (ex) {
     log.error(ex.stack);
