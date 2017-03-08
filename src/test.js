5e9e5a9e11fd1850ae9fd3c3d1e5b96cf1280ec9
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2017-03-08 14:54:04 -0300

    removed coverage

--- src/test.js
+++ src/test.js
@@ -1,19 +1,11 @@
 const debug = require('debug')('apogeu:test');
 const path = require('path');
 const Mocha = require('mocha');
-const istanbul = require('istanbul');
-const im = require('istanbul-middleware');
 
-const logger = require('../src/logger');
 const readDir = require('../src/readDir');
-const getBase = require('../src/getBase');
 
 module.exports = testFolder => new Promise((resolve, reject) => {
   const mocha = new Mocha();
-  const collector = new istanbul.Collector();
-  const reporter = new istanbul.Reporter();
-
-  im.hookLoader(getBase(), { verbose: true });
 
   function addFiles(files) {
     files.forEach((file) => {
@@ -24,12 +16,6 @@ module.exports = testFolder => new Promise((resolve, reject) => {
 
   function execute() {
     mocha.run((failures) => {
-      collector.add(global.__coverage__ || {});
-      reporter.addAll(['text', 'html']);
-      reporter.write(collector, true, () => {
-        logger.info('Coverage report saved to coverage/index.html');
-      });
-
       process.on('exit', () => process.exit(failures));
     });
   }
